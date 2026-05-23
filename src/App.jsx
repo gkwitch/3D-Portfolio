452bf9f6d2f7075254c8de402caba3f08a4e87c0
Gleicy Kethelin <gleicykethelinytb@gmail.com>
2026-05-23 19:42:34 -0300

    studying react, tailwindcss, motion and GSAP

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Header from "./components/Header" //importando o header
+
+export const App = () => {
+  //aqui entraram os componentes
+    return (
+    <>
+    <Header/>
+    </>
+  )
+}
+
+export default App;
